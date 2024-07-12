35e00b8804b9c50f6e2763a7156159b8b72c1d01
linh007 <linh.pham07@base.vn>
2024-07-12 18:39:55 +0700

    update source page

--- lib/screens/sources.dart
+++ lib/screens/sources.dart
@@ -1,31 +1,5 @@
 import 'package:flutter/material.dart';
-
-const Widget divider = SizedBox(height: 20);
-
-enum DatasourceType {
-  machine("Attendance Machine", "machine"),;
-
-  const DatasourceType(this.label, this.code);
-  final String label;
-  final String code;
-}
-
-class Datasource {
-  Datasource(this.sourceType, this.sourceProvider, this.displayName, this.endpoint);
-
-  final String sourceType;
-  final String sourceProvider;
-  final String displayName;
-  final String endpoint;
-}
-
-List<Datasource> _datasources = [
-  Datasource(DatasourceType.machine.label, "ZKTeco", "Văn phòng HN", "192.168.3.101:4370"),
-  Datasource(DatasourceType.machine.label, "Ronald Jack", "Van phong HCM", "192.168.4.101:4370"),
-  Datasource(DatasourceType.machine.label, "Ronald Jack", "Van phong DN", "192.168.5.101:4370"),
-  Datasource(DatasourceType.machine.label, "Ronald Jack", "Van phong HP", "192.168.6.101:4370"),
-  Datasource(DatasourceType.machine.label, "ZKTeco", "Van phong QN", "192.168.7.101:4370"),
-];
+import '../models/sources.dart';
 
 class DatasourceScreen extends StatefulWidget {
   const DatasourceScreen({super.key});
@@ -43,58 +17,11 @@ class _DatasourceState extends State<DatasourceScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     return Expanded(
       child: LayoutBuilder(
         builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: Card(
-              color: Theme.of(context).colorScheme.onSecondary,
-              child: Padding(
-                padding: const EdgeInsets.all(32),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Log sources',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    divider,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const SizedBox(
-                          width: 400,
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.search),
-                              border: OutlineInputBorder(),
-                              labelText: 'Search',
-                              hintText: 'type a display name',
-                            ),
-                          ),
-                        ),
-                        FilledButton.tonalIcon(
-                          onPressed: () {},
-                          label: const Text('New source'),
-                          icon: const Icon(Icons.add),
-                        ),
-                        FloatingActionButton(
-                          foregroundColor: Theme.of(context).colorScheme.primary,
-                          backgroundColor: Theme.of(context).colorScheme.surface,
-                          onPressed: () {},
-                          tooltip: 'Small',
-                          child: const Icon(Icons.settings),
-                        ),
-                      ],
-                    ),
-                    divider,
-                    DataTable(columns: _createColumns(), rows: _createRows()),
-                  ],
-                ),
-              ),
-            ),
+          return const SingleChildScrollView(
+            child: LogSourceDT(),
           );
         },
       ),
@@ -102,22 +29,98 @@ class _DatasourceState extends State<DatasourceScreen> {
   }
 }
 
-List<DataColumn> _createColumns() {
-  return [
-    const DataColumn(label: Text('Name')),
-    const DataColumn(label: Text('Source Type')),
-    const DataColumn(label: Text('Endpoint')),
-    const DataColumn(label: Text('Info')),
-  ];
+class LogSource extends DataTableSource {
+  List<Sources>? data;
+  LogSource({required this.data});
+
+  @override
+  int get rowCount => data?.length ?? 0;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => 0;
+
+  @override
+  DataRow? getRow(int index) {
+    return index <= rowCount
+    ? DataRow(
+      cells: [
+        DataCell(Text(data![index].name)),
+        DataCell(Text(data![index].description ?? "")),
+        const DataCell(Text("status")),
+        const DataCell(Text("d")),
+      ],
+      onSelectChanged: (newValue) {
+        print(newValue);
+      }
+      )
+    : null;
+  }
 }
 
-List<DataRow> _createRows() {
-  return _datasources
-      .map((source) => DataRow(cells: [
-            DataCell(Text(source.displayName)),
-            DataCell(Text(source.sourceType)),
-            DataCell(Text(source.endpoint)),
-            const DataCell(Text('...')),
-          ]))
-      .toList();
+class LogSourceDT extends StatefulWidget {
+  const LogSourceDT({super.key});
+
+  @override
+  State<LogSourceDT> createState() => _LogSourceDTState();
+}
+
+class _LogSourceDTState extends State<LogSourceDT> {
+  List<Sources>? filterSources;
+  int _pageSize = 10;
+
+  @override
+  void initState() {
+    filterSources = mySources;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PaginatedDataTable(
+      availableRowsPerPage: const [5, 10, 25, 50],
+      rowsPerPage: _pageSize,
+      onRowsPerPageChanged: (value) {
+        setState(() {
+          _pageSize = value!;
+        });
+      },
+      showCheckboxColumn: true,
+      header: Container(
+        padding: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black45,
+          ),
+          borderRadius: BorderRadius.circular(8)
+        ),
+        child: TextField(
+          controller: TextEditingController(),
+          decoration: const InputDecoration(hintText: "Enter something to filter"),
+          onChanged: (value) {
+            setState(() {
+              mySources = filterSources!.where((row) => row.name.contains(value)).toList();
+            });
+          },
+        ),
+      ),
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Text('Name'),
+        ),
+        DataColumn(
+          label: Text('Desciption'),
+        ),
+        DataColumn(
+          label: Text('Status'),
+        ),
+        DataColumn(
+          label: Text('#'),
+        ),
+      ],
+      source: LogSource(data: mySources),
+    );
+  }
 }
